0f860a18cdc8022fa7898ec428beb31cd6032b71
amaan35 <amaanlm35@gmail.com>
2024-02-26 16:42:46 +0530

    installing and setting up flowbite react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import About from './pages/About'
-import Dashboard from './pages/Dashboard'
-import Signin from './pages/Signin'
-import Signup from './pages/Signup'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/projects' element={<Projects/>}/>
-        <Route path='/sign-in' element={<Signin/>}/>
-        <Route path='/sign-up' element={<Signup/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
