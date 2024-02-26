6514e8215688ed391d27bf9d3611e78250412ba3
amaan35 <amaanlm35@gmail.com>
2024-02-26 16:18:08 +0530

    initialized pages and added routes to them

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<Signup/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signin = () => {
+  return (
+    <div>
+      SIgn in
+    </div>
+  )
+}
+
+export default Signin
