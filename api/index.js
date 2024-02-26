bb63e5fdfdc7a1a98dc42249b942eae13f807b54
amaan35 <amaanlm35@gmail.com>
2024-02-26 20:47:42 +0530

    creating and Connecting to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
-const app = express()
+dotenv.config();
 
-app.listen(3000,()=>{
-    console.log("Server running on 3000...")
-})
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => {
+    console.log("database is connected...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server running on 3000...");
+});
