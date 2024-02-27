e1728201d31c1ed375fe6b4a99c2b8ebbbbd73df
amaan35 <amaanlm35@gmail.com>
2024-02-28 03:33:29 +0530

    added functionality to the signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,44 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const navigate = useNavigate();
+  const [formdata, setformdata] = useState({});
+  const [errorMessage, seterrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const handleChange = (event) => {
+    setformdata({ ...formdata, [event.target.id]: event.target.value.trim() });
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!formdata.username || !formdata.email || !formdata.password) {
+      seterrorMessage("Please fill out all the fields");
+    }
+    try {
+      setIsLoading(true);
+      seterrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(formdata),
+      });
+      const data = await res.json();
+      //handling error from server
+      if (data.success === false) {
+        return seterrorMessage(data.message);
+      }
+      setIsLoading(false)
+      if(res.ok){
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      //handling error on client side
+      seterrorMessage(error.message);
+    }finally{
+      setIsLoading(false)
+    }
+  };
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -21,25 +58,47 @@ export default function SignUp() {
           </p>
         </div>
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your email" />
               <TextInput
-                type="text"
+                type="email"
                 placeholder="email"
                 id="email"
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value="Your password" />
-              <TextInput type="text" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button className="bg-gradient-to-r from-indigo-500 to-blue-400" type="submit">
-              Sign Up
+            <Button
+              className="bg-gradient-to-r from-indigo-500 to-blue-400"
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -48,6 +107,11 @@ export default function SignUp() {
               Sign In
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
