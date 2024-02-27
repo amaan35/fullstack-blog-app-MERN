3e1f61ec3226f4839860b499d48df583b8910a65
amaan35 <amaanlm35@gmail.com>
2024-02-27 23:24:25 +0530

    Created a middleware in indexjs file to send the error message received from mongoDB as response and also created an errorHandler function to handle our custom errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
-export const signup = async (req, res) => {
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
     !username ||
@@ -10,19 +11,19 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "All fields are required" });
+    next(errorHandler(400, "All fields are required"));
   }
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({
     username,
     email,
-    password : hashedPassword,
+    password: hashedPassword,
   });
 
   try {
     await newUser.save();
     res.json("signup successful");
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
