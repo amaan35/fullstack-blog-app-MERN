cb043958b117ca386d9bd9b537998e23e06e9a83
amaan35 <amaanlm35@gmail.com>
2024-02-28 05:36:01 +0530

    Creating signin page, cookie getting stored in browser, user able to sign in

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,110 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const Signin = () => {
+export default function SignIn() {
+  const navigate = useNavigate();
+  const [formdata, setformdata] = useState({});
+  const [errorMessage, seterrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const handleChange = (event) => {
+    setformdata({ ...formdata, [event.target.id]: event.target.value.trim() });
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!formdata.email || !formdata.password) {
+      seterrorMessage("Please fill out all the fields");
+    }
+    try {
+      setIsLoading(true);
+      seterrorMessage(null);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(formdata),
+      });
+      const data = await res.json();
+      //handling error from server
+      if (data.success === false) {
+        return seterrorMessage(data.message);
+      }
+      setIsLoading(false)
+      if(res.ok){
+        navigate('/')
+      }
+    } catch (error) {
+      //handling error on client side
+      seterrorMessage(error.message);
+    }finally{
+      setIsLoading(false)
+    }
+  };
   return (
-    <div>
-      SIgn in
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          <Link
+            to="/"
+            className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+          >
+            <span className="px-1 py-1 rounded-lg bg-gradient-to-r from-indigo-500 to-blue-400 text-white">
+              Ali's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is a demo project. You can sign in with your email and password
+            or with Google.
+          </p>
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="Your email" />
+              <TextInput
+                type="email"
+                placeholder="email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              className="bg-gradient-to-r from-indigo-500 to-blue-400"
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Dont have an account?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign Up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-export default Signin
