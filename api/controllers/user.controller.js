6fe345d393d275a6a9eced894ccccd580f0783b6
amaan35 <amaanlm35@gmail.com>
2024-03-01 22:01:57 +0530

    Created a route for the user to update their details and verifying the user using their cookie

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,55 @@
-export const test = (req, res)=>{
-    res.json({message: "API working"})
-}
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
+export const test = (req, res) => {
+  res.json({ message: "API working" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(
+      errorHandler(403, "You are not authorized to edit this information")
+    );
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 8) {
+      return next(errorHandler(400, "Password must be at least 8 digits long"));
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(
+          400,
+          "Username should be between 7 to 20 characters"
+        )
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Spaces are not allowed in the username"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "username must be lowercase"));
+    }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        {
+          $set: {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            profilePicture: req.body.profilePicture,
+          },
+        },
+        { new: true }
+      );
+      const {password, ...rest} = updatedUser._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  }
+};
