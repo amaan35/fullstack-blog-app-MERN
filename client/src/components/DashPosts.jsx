4bc8343d1e73bee0d9511c0d70a57db86e08c2a3
amaan35 <amaanlm35@gmail.com>
2024-03-05 08:43:26 +0530

    Initializing post tab of dashboard

---
+++ client/src/components/DashPosts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashPosts = () => {
+  return (
+    <div>
+      Dash Posts
+    </div>
+  )
+}
+
+export default DashPosts
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -16,11 +17,10 @@ export default function Dashboard() {
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className='md:w-56'>
-        {/* Sidebar */}
         <DashSidebar />
       </div>
-      {/* profile... */}
       {tab === 'profile' && <DashProfile />}
+      {tab === 'posts' && <DashPosts/>}
     </div>
   );
 }
