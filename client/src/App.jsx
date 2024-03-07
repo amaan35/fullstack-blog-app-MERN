b17519f7e2184aa3313b6db3c1866fec7fd678c3
amaan35 <amaanlm35@gmail.com>
2024-03-07 06:26:04 +0530

    Creating post page and showing the data in it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 
 export default function App() {
   return (
@@ -30,6 +31,8 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
+
       </Routes>
       <FooterComp />
     </BrowserRouter>
