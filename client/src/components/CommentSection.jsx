de25fe9ca6067f39f2bc72ea555c73c58122af24
amaan35 <amaanlm35@gmail.com>
2024-03-08 04:10:41 +0530

    Creating API route for liking a comment and showing the likes in UI

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,10 +1,11 @@
 import { Alert, Button, Textarea } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Comment from "./Comment";
 
 export default function CommentSection({ postId }) {
+  const navigate = useNavigate();
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
@@ -48,6 +49,33 @@ export default function CommentSection({ postId }) {
       }
     } catch (error) {
       setCommentError(error.message);
+    }
+  };
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`/api/comment/likecomment/${commentId}`, {
+        method: "PUT",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) => 
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
     }
   };
   return (
@@ -113,7 +141,13 @@ export default function CommentSection({ postId }) {
             </div>
           </div>
           {comments.map((comment) => {
-            return <Comment key={comment._id} comment={comment}/>;
+            return (
+              <Comment
+                key={comment._id}
+                comment={comment}
+                onLike={handleLike}
+              />
+            );
           })}
         </>
       )}
